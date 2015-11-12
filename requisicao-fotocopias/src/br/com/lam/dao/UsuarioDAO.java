d7080bf2fd9de6defb6d5c1eaa0ccfc31030c409
Lázaro <lazarovasconceloscoutinho@gmail.com>
2015-11-12 20:24:22 -0300

    Adicionando classes DAO implementadas pelo Allyson

---
+++ requisicao-fotocopias/src/br/com/lam/dao/UsuarioDAO.java
@@ -0,0 +1,51 @@
+package br.com.lam.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.lam.model.Usuario;
+
+public class UsuarioDAO extends DAO implements GenericDAO<Usuario>{
+
+	public UsuarioDAO(EntityManager em) {
+		super(em);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void salva(Usuario e) {
+		em.persist(e);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Usuario pesquisa(long id) {
+		// TODO Auto-generated method stub
+		return em.find(Usuario.class, id);
+	}
+
+	@Override
+	public void exclui(Usuario e) {
+		em.remove(e);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualiza(Usuario e) {
+		em.merge(e);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Usuario> lista() {
+		// TODO Auto-generated method stub
+		return em.createQuery("select u from Usuario u", Usuario.class).getResultList();
+	}
+	
+	
+
+}
