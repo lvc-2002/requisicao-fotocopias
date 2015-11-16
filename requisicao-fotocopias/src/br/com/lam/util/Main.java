f44cdece3669fd4b07f66f8eb9f003fe76202638
Lázaro <lazarovasconceloscoutinho@gmail.com>
2015-11-16 11:50:36 -0300

    Adicionando alterações

---
+++ requisicao-fotocopias/src/br/com/lam/util/Main.java
@@ -0,0 +1,13 @@
+package br.com.lam.util;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		/*Usuario u = new UsuarioDAO(JPAUtil.getEntityManager()).pesquisa("12345", "1234");
+		System.out.println(u);*/
+		
+		//System.out.println(Sexo.F.valor);
+		
+	}
+
+}
