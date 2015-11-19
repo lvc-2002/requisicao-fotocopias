939e440c94be3447795e8040efd1c9c685108ade
Lázaro <lazarovasconceloscoutinho@gmail.com>
2015-11-19 15:47:20 -0300

    Adicionando Exceção

---
+++ requisicao-fotocopias/src/br/com/lam/regranegocio/UsuarioException.java
@@ -0,0 +1,11 @@
+package br.com.lam.regranegocio;
+
+public class UsuarioException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public UsuarioException(String msg) {
+		super(msg);
+	}
+
+}
