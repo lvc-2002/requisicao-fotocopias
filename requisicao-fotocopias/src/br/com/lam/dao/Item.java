09b39c1fb29bc5d89c3cdabed2d2002004da6bc6
Lázaro <lazarovasconceloscoutinho@gmail.com>
2015-12-10 15:11:34 -0300

    Adicionando o código das requisições no formato 'yyyy/xxxxxx'.

--- requisicao-fotocopias/src/br/com/lam/dao/Item.java
+++ requisicao-fotocopias/src/br/com/lam/dao/Item.java
@@ -8,36 +8,30 @@ public class Item extends DAO implements GenericDAO<Item> {
 
 	public Item(EntityManager em) {
 		super(em);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void salva(Item e) {
-		// TODO Auto-generated method stub
 		em.persist(e);
 	}
 
 	@Override
 	public Item pesquisa(long id) {
-		// TODO Auto-generated method stub
 		return em.find(Item.class, id);
 	}
 
 	@Override
 	public void exclui(Item e) {
-		// TODO Auto-generated method stub
 		em.remove(e);
 	}
 
 	@Override
 	public void atualiza(Item e) {
-		// TODO Auto-generated method stub
 		em.merge(e);
 	}
 
 	@Override
 	public List<Item> lista() {
-		// TODO Auto-generated method stub
 		return em.createQuery("select i from Item i", Item.class).getResultList();
 	}
 
